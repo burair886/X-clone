626f71e9624bc81d6a332de7c1c7c327ce0c7703
burair886 <64698373+burair886@users.noreply.github.com>
2023-12-17 05:01:06 +0500

    feat : basic layout

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="min-h-full" :class="{ dark: darkMode }">
+    <div class="flex mx-auto sm:px-6 lg:px-8 lg:gap-5 lg:max-w-7xl">
+      <!-- Left Sidebar -->
+      <div class="hidden md:block xs:columns-1 lg:columns-4 bg-red-600">
+        <div class="sticky top-0">
+          <div class="h-8 w-8">
+            <AssetsLogoTwitterX />
+          </div>
+        </div>
+      </div>
+      <!-- Main page -->
+      <section>
+        <slot />
+      </section>
+      <!-- Right Sidebar -->
+      <section>right</section>
+    </div>
+  </div>
+</template>
+<script setup>
+import AssetsLogoTwitterX from "../assets/logo/twitterX.vue";
+const darkMode = ref(true);
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div :class="{ dark: darkMode }">
+    <div class="text-red-800 dark:text-dim-500">hello</div>
+  </div>
+</template>
+<script setup>
+const darkMode = ref(false);
+</script>
