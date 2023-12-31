73461dcd3a6d892e2e2962cc185f21c680a17594
burair886 <64698373+burair886@users.noreply.github.com>
2023-12-31 18:06:09 +0500

    feat: main Section (in progress)

---
+++ components/sections/main.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>{{ props.title }}</h1>
+    <slot />
+  </div>
+</template>
+<script setup>
+const props = defineProps({
+  title: {
+    type: String,
+    require: true,
+  },
+  loading: {
+    type: Boolean,
+    require: true,
+  },
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <div class="text-red-800 dark:text-dim-500">hello</div>
+    <div class="text-red-800 dark:text-dim-500">
+      <section-main title="Main" :loading="false">  </section-main>
+    </div>
   </div>
 </template>
 <script setup></script>
